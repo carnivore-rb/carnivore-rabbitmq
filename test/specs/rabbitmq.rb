2ceb5d07efb399ba62d5d10fe3ba9085d8fedf34
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-13 05:36:41 -0700

    Generate random names. Add round trip test.

--- test/specs/rabbitmq.rb
+++ test/specs/rabbitmq.rb
@@ -1,5 +1,9 @@
 require 'carnivore-rabbitmq'
 
+def generate_name(prefix)
+  "#{prefix}-#{Celluloid.uuid}"
+end
+
 describe Carnivore::Source::Rabbitmq do
 
   describe 'Building a Rabbitmq based source' do
@@ -15,45 +19,64 @@ describe Carnivore::Source::Rabbitmq do
             :username => ENV.fetch('CARNIVORE_RABBIT_USERNAME', 'guest'),
             :password => ENV.fetch('CARNIVORE_RABBIT_PASSWORD', 'guest')
           },
-          :queue => 'crnvr-q',
-          :exchange => 'crnvr-xg'
+          :queue => generate_name(:queue),
+          :exchange => generate_name(:exchange)
         }
       )
       t = Thread.new{ Carnivore.start! }
       source_wait
       Carnivore::Supervisor.supervisor[:rabbitmq_source].name.wont_be_nil
       t.terminate
+      Carnivore::Supervisor.supervisor.terminate!
     end
 
   end
-=begin
-  describe 'Actor source based communication' do
+
+  describe 'Rabbitmq source based communication' do
     before do
       @source1 = []
       @source2 = []
       MessageStore.init
-      Carnivore::Source.build(:type => :actor, :args => {:name => :actor_source}).add_callback(:store) do |message|
-        MessageStore.messages.push(message[:message])
+      Carnivore::Source.build(
+        :type => :rabbitmq,
+        :args => {
+          :name => :rabbitmq_source,
+          :connection => {
+            :host => ENV.fetch('CARNIVORE_RABBIT_HOST', '127.0.0.1'),
+            :port => ENV.fetch('CARNIVORE_RABBIT_PORT', 5672),
+            :username => ENV.fetch('CARNIVORE_RABBIT_USERNAME', 'guest'),
+            :password => ENV.fetch('CARNIVORE_RABBIT_PASSWORD', 'guest')
+          },
+          :queue => generate_name(:queue),
+          :exchange => generate_name(:exchange)
+        }
+      ).add_callback(:store) do |message|
+        MessageStore.messages.push(message[:message][:payload])
       end
       @runner = Thread.new{ Carnivore.start! }
-      source_wait
+      source_wait do
+        Carnivore::Supervisor.supervisor &&
+          Carnivore::Supervisor.supervisor.alive? &&
+          !Carnivore::Supervisor.supervisor[:rabbitmq_source].nil?
+      end
     end
 
     after do
       @runner.terminate
+      Carnivore::Supervisor.supervisor.terminate!
     end
 
     describe 'message transmissions' do
       it 'should accept message transmits' do
-        Carnivore::Supervisor.supervisor[:actor_source].transmit('test message')
+        Carnivore::Supervisor.supervisor[:rabbitmq_source].transmit('test message')
       end
 
       it 'should receive messages' do
-        Carnivore::Supervisor.supervisor[:actor_source].transmit('test message 2')
+        Carnivore::Supervisor.supervisor[:rabbitmq_source].transmit('test message 2')
         source_wait{ MessageStore.messages.include?('test message 2') }
         MessageStore.messages.must_include 'test message 2'
       end
     end
   end
-=end
+
 end
