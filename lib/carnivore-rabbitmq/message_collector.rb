4285a6ac232996518070b540818db9422cda2ea3
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-12 22:08:32 -0700

    Fill in method info within collector

--- lib/carnivore-rabbitmq/message_collector.rb
+++ lib/carnivore-rabbitmq/message_collector.rb
@@ -1,19 +1,33 @@
 module Carnivore
   class Source
     class Rabbitmq
+      # Message collector
       class MessageCollector
 
         include Celluloid
         include Carnivore::Utils::Logging
 
-        attr_reader :queue, :message_queue, :notify
+        # @return [Bunny::Queue, MarchHare::Queue] remote queue
+        attr_reader :queue
+        # @return [Queue] local message bucket
+        attr_reader :message_queue
+        # @return [Celluloid::Actor] actor to notify
+        attr_reader :notify
 
+        # Create new instance
+        #
+        # @param queue [Bunny::Queue, MarchHare::Queue] remote queue
+        # @param message_queue [Queue] local message bucket
+        # @param notify [Celluloid::Actor] actor to notify
         def initialize(queue, message_queue, notify)
           @queue = queue
           @message_queue = message_queue
           @notify = notify
         end
 
+        # Collect messages from remote queue
+        #
+        # @return [TrueClass]
         def collect_messages
           queue.subscribe(:block => true, :ack => true) do |info, metadata, payload|
             debug "Message received: #{payload.inspect}"
@@ -22,6 +36,7 @@ module Carnivore
             message_queue << {:info => info, :metadata => metadata, :payload => payload}
             notify.signal(:new_messages)
           end
+          true
         end
 
       end
