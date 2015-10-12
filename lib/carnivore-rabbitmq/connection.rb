365f3f3db39d9dce24ad2f063d1dbd42e8b9b587
Chris Roberts <code@chrisroberts.org>
2015-10-12 14:01:27 -0700

    Refactor to isolate connection for easier supervised replacement

---
+++ lib/carnivore-rabbitmq/connection.rb
@@ -0,0 +1,134 @@
+require 'carnivore-rabbitmq'
+
+module Carnivore
+  class Source
+    class Rabbitmq
+      # RabbitMQ connection
+      class Connection
+
+        include Zoidberg::SoftShell
+        include Zoidberg::Supervise
+        include Carnivore::Utils::Logging
+
+        # @return [Hash]
+        attr_reader :source_args
+        # @return [String]
+        attr_reader :routing_key
+        # @return [Bunny::Connection, MarchHare::Connection]
+        attr_reader :connection
+        # @return [Bunny::Channel, MarchHare::Channel]
+        attr_reader :channel
+        # @return [Bunny::Exchange, MarchHare::Exchange]
+        attr_reader :exchange
+        # @return [Bunny::Queue, MarchHare::Queue]
+        attr_reader :queue
+        # @return [Carnivore::Source::Rabbitmq]
+        attr_reader :source
+        # @return [Queue]
+        attr_reader :message_queue
+
+        # Create new conection
+        #
+        # @param r_source [Carnivore::Source::Rabbitmq] origin
+        # @param m_queue [Queue] common message queue
+        # @return [self]
+        def initialize(r_source, m_queue)
+          @source = r_source
+          @source_args = r_source.args.to_smash
+          @message_queue = m_queue
+          connect
+        end
+
+        # Restart trigger for supervised replacements
+        def restarted
+          connect
+          if(source.collect_messages)
+            async.receive_messages
+          end
+        end
+
+        # Establish connection to remote server and setup
+        #
+        # @return [MarchHare::Session, Bunny::Session]
+        def connect
+          unless(source_args[:connection])
+            abort KeyError.new "No configuration defined for connection type (#{connection_library})"
+          end
+          connection_args = Carnivore::Utils.symbolize_hash(source_args[:connection])
+          case connection_library
+          when :bunny
+            require 'bunny'
+            @connection = Bunny.new(connection_args)
+          when :march_hare
+            require 'march_hare'
+            @connection = MarchHare.connect(connection_args)
+          else
+            abort ArgumentError.new("No valid connection arguments defined (:bunny or :march_hare must be defined)")
+          end
+          connection.start
+          @routing_key = source_args[:routing_key]
+          @channel = connection.create_channel
+          @exchange = channel.topic(source_args[:exchange])
+          @queue = channel.queue(source_args[:queue], :auto_delete => false).
+            bind(exchange, :routing_key => routing_key)
+          @connection
+        end
+
+        # Write message to connection
+        #
+        # @param payload [String]
+        def write(payload)
+          if(source_args[:publish_via].to_s == 'exchange')
+            exchange.publish(payload, :routing_key => routing_key)
+          else
+            queue.publish(payload, :routing_key => routing_key)
+          end
+        end
+
+        # Send message acknowledgement
+        #
+        # @param tag [String]
+        def ack(tag)
+          channel.acknowledge(tag, false)
+        end
+
+        # Start receiving message
+        def receive_messages
+          queue.subscribe(:block => true, :manual_ack => true) do |info, metadata, payload|
+            if(payload.nil?)
+              payload = metadata
+              metadata = {}
+            end
+            begin
+              payload = MultiJson.load(payload).to_smash
+            rescue MultiJson::ParseError
+              debug 'Received payload not in JSON format. Failed to parse!'
+            end
+            debug "Message received: #{payload.inspect}"
+            debug "Message info: #{info.inspect}"
+            debug "Message metadata: #{metadata.inspect}"
+            new_message = Smash.new(
+              :raw => Smash.new(
+                :info => info,
+                :metadata => metadata
+              ),
+              :content => payload
+            )
+            message_queue.push new_message
+          end
+          true
+        end
+
+        # @return [Symbol] connection library to utilize
+        def connection_library
+          if(source_args[:force_library])
+            source_args[:force_library].to_sym
+          else
+            RUBY_PLATFORM == 'java' ? :march_hare : :bunny
+          end
+        end
+
+      end
+    end
+  end
+end
--- lib/carnivore-rabbitmq/rabbitmq.rb
+++ lib/carnivore-rabbitmq/rabbitmq.rb
@@ -6,24 +6,16 @@ module Carnivore
     # RabbitMQ based carnivore source
     class Rabbitmq < Source
 
-      option :cache_signals
-
-      autoload :MessageCollector, 'carnivore-rabbitmq/message_collector'
+      autoload :Connection, 'carnivore-rabbitmq/connection'
 
       # @return [Smash] initialization arguments
       attr_reader :args
-      # @return [MarchHare::Session, Bunny::Session] current connection
+      # @return [Connection] current connection
       attr_reader :connection
-      # @return [MarchHare::Exchange, Bunny::Exchange] current exchange
-      attr_reader :exchange
-      # @return [MarchHare::Channel, Bunny::Channel] current channel
-      attr_reader :channel
-      # @return [MarchHare::Queue, Bunny::Queue] current queue
-      attr_reader :queue
-      # @return [String] routing key
-      attr_reader :routing_key
-      # @return [Carnviore::Source::Rabbitmq::MessageCollector] message collector
-      attr_reader :message_collector
+      # @return [Queue]
+      attr_reader :message_queue
+      # @return [TrueClass, FalseClass]
+      attr_reader :collect_messages
 
       # RabbitMQ source setup
       #
@@ -34,68 +26,32 @@ module Carnivore
       # @option init_args [String, Symbol] :force_library :bunny or :march_hare
       def setup(init_args={})
         @args = args.dup
-        @queue_name = args[:queue]
-        @exchange_name = args[:exchange]
+        @message_queue = Queue.new
         debug "Creating Rabbitmq source instance <#{name}>"
       end
 
       # Connect to the remote server
       def connect
-        establish_connection
-      end
-
-      # Start the message collection
-      def start_collector
-        unless(@collecting)
-          @message_collector = MessageCollector.new(queue, current_actor)
-          message_collector.start!
-        end
+        @connection = Connection.new(current_self, message_queue)
       end
 
       # Destroy message collector
       #
       # @return [TrueClass]
       def terminate
-        connection.close if connection
-        if(message_collector && message_collector.alive?)
-          message_collector.terminate
-        end
+        connection.terminate if connection && connection.alive?
         true
-      end
-
-      # Establish connection to remote server and setup
-      #
-      # @return [MarchHare::Session, Bunny::Session]
-      def establish_connection
-        unless(args[:connection])
-          abort KeyError.new "No configuration defined for connection type (#{connection_library})"
-        end
-        connection_args = Carnivore::Utils.symbolize_hash(args[:connection])
-        case connection_library
-        when :bunny
-          require 'bunny'
-          @connection = Bunny.new(connection_args)
-        when :march_hare
-          require 'march_hare'
-          @connection = MarchHare.connect(connection_args)
-        else
-          abort ArgumentError.new("No valid connection arguments defined (:bunny or :march_hare must be defined)")
-        end
-        connection.start
-        @routing_key = args[:routing_key]
-        @channel = connection.create_channel
-        @exchange = channel.topic(args[:exchange])
-        @queue = channel.queue(args[:queue], :auto_delete => false).
-          bind(exchange, :routing_key => routing_key)
-        @connection
       end
 
       # Receive payload from connection
       #
       # @return [Hash] payload
       def receive(*_)
-        start_collector
-        wait(:new_messages)
+        unless(collect_messages)
+          @collect_messages = true
+          connection.async.receive_messages
+        end
+        defer{ message_queue.pop }
       end
 
       # Transmit payload to connection
@@ -103,11 +59,7 @@ module Carnivore
       # @param payload [Object]
       def transmit(payload, *_)
         payload = MultiJson.dump(payload) unless payload.is_a?(String)
-        if(args[:publish_via].to_s == 'exchange')
-          exchange.publish(payload, :routing_key => routing_key)
-        else
-          queue.publish(payload, :routing_key => routing_key)
-        end
+        defer{ connection.write(payload) }
       end
 
       # Confirm message processing
@@ -115,16 +67,7 @@ module Carnivore
       # @param message [Carnivore::Message]
       def confirm(message)
         info "Confirming message #{message}"
-        channel.acknowledge(message[:message][:info].delivery_tag, false)
-      end
-
-      # @return [Symbol] connection library to utilize
-      def connection_library
-        if(args[:force_library])
-          args[:force_library].to_sym
-        else
-          RUBY_PLATFORM == 'java' ? :march_hare : :bunny
-        end
+        defer{ connection.ack(message[:message][:info].delivery_tag) }
       end
 
     end
